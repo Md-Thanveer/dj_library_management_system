7c3a14a16b353be2c2624d72abaeecaef981e8c6
MOHAMED THANVEER <thanveerami612@gmail.com>
2025-04-27 13:11:01 +0530

    Book Added To Admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from backend.models import Category, Author
+from backend.models import Category, Author, Book
 
 
 # Register your models here.
@@ -11,3 +11,12 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     search_fields = ('first_name', 'last_name',)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'publication_date', 'copies_owned')
+
+    search_fields = ('title',)
+
+    list_filter = ('category', 'publication_date')
