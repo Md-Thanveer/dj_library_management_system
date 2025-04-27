c5d9431e29cd1cbb6196f163b2b23a275db78b78
MOHAMED THANVEER <thanveerami612@gmail.com>
2025-04-27 13:09:13 +0530

    Author added to admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from backend.models import Category
+from backend.models import Category, Author
 
 
 # Register your models here.
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     search_fields = ('name',)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ('first_name', 'last_name',)
